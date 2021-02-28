a3f40f18ef563f5b0b1110220aaee01ef093b23e
rogerio <rogerio_calculo@hotmail.com>
2021-02-28 09:05:32 -0300

    Add_twwetutilities

---
+++ tweetutilities.py
@@ -0,0 +1,15 @@
+
+
+def print_tweets(tweets):
+    """For each Tweepy Status object in tweets, display the
+    user's screen_name and tweet text. If the language is not
+    English, translate the text with TextBlob."""
+    for tweet in tweets:
+        print(f'{tweet.screen_name}:', end=' ')
+        
+        if 'en' in tweet.lang:
+            print(f'{tweet.text}\n')
+        elif 'und' not in tweet.lang: # translate to English first
+            print(f'\n ORIGINAL: {tweet.text}')
+            print(f'TRANSLATED: {TextBlob(tweet.text).translate()}\n')
+     
