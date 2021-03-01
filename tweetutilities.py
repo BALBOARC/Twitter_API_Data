2677dbe31cf87b07070c1c9584dab335451896de
rogerio <rogerio_calculo@hotmail.com>
2021-02-28 21:34:39 -0300

    wordcloud

--- tweetutilities.py
+++ tweetutilities.py
@@ -1,11 +1,9 @@
-
-
 def print_tweets(tweets):
     """For each Tweepy Status object in tweets, display the
     user's screen_name and tweet text. If the language is not
     English, translate the text with TextBlob."""
     for tweet in tweets:
-        print(f'{tweet.screen_name}:', end=' ')
+        # print(f'{tweet.screen_name}:', end=' ')
         
         if 'en' in tweet.lang:
             print(f'{tweet.text}\n')
